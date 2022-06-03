6f423def3f137a10c899e614b5c00eccf3f84fdf
Dr. Jerry A. Smith <jerry@drjerryasmith.com>
2022-06-03 12:51:03 -0400

    update

--- alert.py
+++ alert.py
@@ -3,47 +3,50 @@ import pandas_ta as ta
 import ccxt, yfinance
 import requests
 
+
 exchange = ccxt.binance()
 
-bars = exchange.fetch_ohlcv("ETH/USDT", timeframe = "5m", limit = 500)
+symbols = pd.DataFrame(symbols, columns = ["symbol"])
+symbols = dft2["symbol"].values.tolist()
 
-df = pd.DataFrame(bars, columns = ["time", "open", "high", "low", "close", "volume"])
+for symbol in symbols:
+    bars = exchange.fetch_ohlcv(symbol, timeframe = "5m", limit = 500)
+    df = pd.DataFrame(bars, columns = ["time", "open", "high", "low", "close", "volume"])
 
+    # technical indicators
+    adx = df.ta.adx()
+    macd = df.ta.macd(fast = 14, slow = 28)
+    rsi = df.ta.rsi()
 
-adx = df.ta.adx()
-macd = df.ta.macd(fast = 14, slow = 28)
-rsi = df.ta.rsi()
+    # create composite df
+    df = pd.concat([df, adx, macd, rsi], axis = 1)
 
-df = pd.concat([df, adx, macd, rsi], axis = 1)
+    # grab the last value
+    last_row = df.iloc[-1]
 
-last_row = df.iloc[-1]
+    # hook to discord channel
+    WEBHOOK_URL = "https://discord.com/api/webhooks/981984523317301279/AZEt7Fz5WWZoHqUgaKfczzmj7Mjf7XuumJLLss-nEFuSZ6FWE_dUWgqvzLQpTmBAoxz1"
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/982018197245022300/1vYv70MN3smSOQKNUmfIaImQJKDMFa5fl1NE74vRqBHifK5Bqim2z7vbfMGZnYQFWohi"
+    # logic
+    if last_row["ADX_14"] > 25:
+        if last_row["DMP_14"] > last_row["DMN_14"]:
+            message = f"STRONG UP TREND: {symbol}: The ADX is {last_row['ADX_14']: .2f} +DI {last_row['DMP_14']: .2f} -DI {last_row['DMN_14']: .2f}"
+        if last_row["DMN_14"] > last_row["DMP_14"]:
+            message = f"STRONG DOWN TREND:{symbol}: The ADX is {last_row['ADX_14']: .2f} +DI {last_row['DMP_14']: .2f} -DI {last_row['DMN_14']: .2f}"
 
-tmp = last_row["ADX_14"]
-if last_row["ADX_14"] > 25:
-    if last_row["DMP_14"] > last_row["DMN_14"]:
-        message = f"STRONG UP TREND: The ADX is {last_row['ADX_14']: .2f} +DI {last_row['DMP_14']} -DI {last_row['DMN_14']}"
-    if last_row["DMN_14"] > last_row["DMP_14"]:
-        message = f"STRONG DOWN TREND: The ADX is {last_row['ADX_14']} +DI {last_row['DMP_14']} -DI {last_row['DMN_14']}"
-    
-    payload = {
-        "username" : "alertbot",
-        "content" : message
-    }
-    
-    requests.post(WEBHOOK_URL, json = payload)
+        payload = {
+            "username" : "alertbot",
+            "content" : message
+        }
 
-if last_row["ADX_14"] < 25:
-    message = f"NO TREND: The ADX is {last_row['ADX_14']} +DI {last_row['DMP_14']} -DI {last_row['DMN_14']}"
-    print(message)
-    
-    payload = {
-        "username" : "alertbot",
-        "content" : message
-    }
-    
-    requests.post(WEBHOOK_URL, json = payload)
+        requests.post(WEBHOOK_URL, json = payload)
 
+    if last_row["ADX_14"] < 25:
+        message = f"NO TREND:{symbol}: The ADX is {last_row['ADX_14']: .2f} +DI {last_row['DMP_14']: .2f} -DI {last_row['DMN_14']: .2f}"
 
-
+        payload = {
+            "username" : "alertbot",
+            "content" : message
+        }
+        
+        requests.post(WEBHOOK_URL, json = payload)
